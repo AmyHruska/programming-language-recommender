22e14211961098a05a9c9fd02e07cea2f8d5340a
AmyHruska <amyhruskacda@gmail.com>
2023-11-04 16:15:51 -0700

    complete MVP

--- changelog.md
+++ changelog.md
@@ -1,14 +1,21 @@
 # Programming language recommender
-### Version of: 17:21 PDT 3-Nov-2024
-
 ## Change log:
 ***
+### Version as of: 17:21 PDT 3-Nov-2024:
 * added change log?!
 * filled in basic content for index.html 95% complete for now
 * finalized styles.css
 * added eventual document.body.remove button. LOL
-## Next versions goals are:
+### Next versions goals are:
 * begin basic java script logic
 * complete index.html
 * Improve my understanding of markdown 
-
+***
+### Version as of: 14:18 PDT 4-Nov-2024:
+* abandoned document.body.remove idea, may revisit
+* began javascript functionality
+* completed index.html
+### Next versions goals are:
+* attempt to get the if than completed for the questions part of the assignment
+***
+### Version as of:
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,43 @@
 //Business logic
-function getResult(){
-    let q2 = parseInt(getElementById("food").value);
-    let q1 = parseInt(getElementById("superpowers").value);
-    let q3 = parseInt(getElementById("hddb").value);
-    let q4 = parseInt(getElementById("dvw").value);
-    let q5 = parseInt(getElementById("w1d1").value);
-    let formResult = q1 + q2 + q3 + q4 + q5;
-    // I feel like form result should be more complicated... 
-}
 
 
 //UI logic
-window.addEventListener("load", function() {
-    let dont = document.querySelector("buttonDont#dont");
-    function nuke() {
-        dont.addEventListener("click", function 
-            document.body.remove();
-        )}
+window.onload =  function() {
+    let form = document.getElementById("questions");
+    let formResult = ("");
+    let resultStyle = document.getElementById("results");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let q1 = parseInt(document.getElementById("superpowers").value);
+        let q2 = parseInt(document.getElementById("food").value);
+        let q3 = parseInt(document.getElementById("hddb").value);
+        let q4 = parseInt(document.getElementById("dvw").value);
+        let q5 = parseInt(document.getElementById("w1d1").value);
+        let formResult = q1 + q2 + q3 + q4 + q5; 
+        
+        if (q1 && q2 && q3 && q4 && q5){
+            if (formResult === 26) {
+                document.getElementById("resultAnswer").innerText = "Python"
+                resultStyle.removeAttribute("class");
+                document.getElementById("warning").setAttribute("class", "hidden");
+            } else if (formResult < 26 && formResult >= 15) {
+                document.getElementById("resultAnswer").innerText = "Java"
+                resultStyle.removeAttribute("class");
+                document.getElementById("warning").setAttribute("class", "hidden");
+            } else if (formResult < 15 || formResult >= 5 ) {
+                document.getElementById("resultAnswer").innerText = "PHP"
+                resultStyle.removeAttribute("class");
+                document.getElementById("warning").setAttribute("class", "hidden");
+            } else if (formResult < 5) {
+                document.getElementById("resultAnswer").innerText = "Quality Assurance testing"
+                resultStyle.removeAttribute("class");
+                document.getElementById("warning").setAttribute("class", "hidden");
+            }
+        } else {
+            document.getElementById("warning").removeAttribute("class");
+            resultStyle.setAttribute("class", "hidden");
+            
+        }
     
-});
-
-
-  //
+    });
+}
